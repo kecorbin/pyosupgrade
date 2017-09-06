235b5ea296f461230449f7466fba1f2e128e2bcc
kecorbin <kecorbin@cisco.com>
2017-09-06 16:48:15 -0500

    bugfix for log file name generation

--- pyosupgrade/views/logbin.py
+++ pyosupgrade/views/logbin.py
@@ -1,13 +1,23 @@
 import os
 from flask import request, url_for, render_template
 from flask_restful import Resource, abort
-
+import errno
 
 class LogFile(object):
 
     def __init__(self, text):
+        logdir = os.getcwd() + '/logs'
+        # make logs directory if one does not exist
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
         files = os.listdir('{}/logs'.format(os.getcwd()))
-        self.id = max(map(lambda x: int(x.split('.')[0]), files)) + 1
+        # only concerned with numbered files e.g 1.log
+        files = [f for f in files if f.split('.')[0].isdigit()]
+        try:
+            self.id = max(map(lambda x: int(x.split('.')[0]), files)) + 1
+        # ValueError is raised if no numered files are found (dir is empty)
+        except ValueError:
+            self.id = 0
         fh = open('./logs/{}.log'.format(self.id), 'w+')
         fh.write(text)
         fh.close()
