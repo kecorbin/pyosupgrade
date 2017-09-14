593b7ffdf85b56bb7634f96aab847f461e1695e2
kecorbin <kecorbin@cisco.com>
2017-09-14 11:02:43 -0500

    use our fork of flower

--- flowerconfig.py
+++ flowerconfig.py
@@ -2,6 +2,7 @@ from flower.utils.template import humanize
 from ast import literal_eval
 
 def format_task(task):
+    print "sanitizing ui output"
     task.args = literal_eval(task.args)[1]
     task.result = humanize(task.result, length=20)
     return task
