b60ce49e66de86bd08ba89135d9d66c51cff201e
kecorbin <kecorbin@cisco.com>
2017-09-14 12:37:58 -0500

    change format_task per requirements

--- flowerconfig.py
+++ flowerconfig.py
@@ -3,6 +3,7 @@ from ast import literal_eval
 
 def format_task(task):
     print "sanitizing ui output"
-    task.args = literal_eval(task.args)[1]
-    task.result = humanize(task.result, length=20)
+    args_tuple = literal_eval(task.args)
+    task.args = args_tuple[1]
+    task.result = "{} upgrade task details at {}".format(args_tuple[1], args_tuple[0])
     return task
